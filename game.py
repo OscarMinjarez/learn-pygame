a5a35b76a44294667c8893dea6d1c51e936298c9
OscarMinjarez <oscarminjarez@outlook.com>
2022-09-23 20:38:35 -0700

    I have used rectangles for the position of images

--- game.py
+++ game.py
@@ -7,13 +7,15 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("./graphics/Sky.png")
-ground_surface = pygame.image.load("./graphics/ground.png")
+sky_surface = pygame.image.load("./graphics/Sky.png").convert_alpha()
+ground_surface = pygame.image.load("./graphics/ground.png").convert_alpha()
 text_surface = test_font.render("My game", False, "Black")
 
-snail_surface = pygame.image.load("./graphics/snail/snail1.png")
-snail_x_position = 600
-snail_y_position = 250
+snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+
+player_surface = pygame.image.load("./graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
 	for event in pygame.event.get():
@@ -25,9 +27,15 @@ while True:
 	screen.blit(ground_surface, (0, 300))
 	screen.blit(text_surface, (300, 50))
 
-	snail_x_position -= 4 
+	snail_rect.x -= 4
 
-	screen.blit(snail_surface, (snail_x_position, snail_y_position))
+	if snail_rect.right < 0:
+		snail_rect.left = 800	
+
+	screen.blit(snail_surface, snail_rect)
+
+	player_rect.left += 1
+	screen.blit(player_surface, player_rect)
 
 	pygame.display.update()
 	clock.tick(60)
