8994f8955bdda93a54bb14e1a0e2c35bdd0d59c4
OscarMinjarez <oscarminjarez@outlook.com>
2022-09-30 08:32:04 -0700

    Pygame.draw was used to add elements to the screen

--- game.py
+++ game.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("./graphics/Sky.png").convert_alpha()
 ground_surface = pygame.image.load("./graphics/ground.png").convert_alpha()
-text_surface = test_font.render("My game", False, "Black")
+
+score_surface = test_font.render("My game", False, (64, 64, 64))
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600, 300))
@@ -22,21 +24,21 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
-		if event.type == pygame.MOUSEMOTION:
-			if player_rect.collidepoint(event.pos):
-				print("collision")
+		# if event.type == pygame.MOUSEMOTION:
+		# 	if player_rect.collidepoint(event.pos):
+		# 		print("collision")
 
 	screen.blit(sky_surface, (0, 0))
 	screen.blit(ground_surface, (0, 300))
-	screen.blit(text_surface, (300, 50))
+	pygame.draw.rect(screen, "#c0e8ec", score_rect)
+	pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+	screen.blit(score_surface, score_rect)
 
 	snail_rect.x -= 4
-
 	if snail_rect.right < 0:
 		snail_rect.left = 800	
 
 	screen.blit(snail_surface, snail_rect)
-
 	screen.blit(player_surface, player_rect)
 
 	# if player_rect.colliderect(snail_rect):
